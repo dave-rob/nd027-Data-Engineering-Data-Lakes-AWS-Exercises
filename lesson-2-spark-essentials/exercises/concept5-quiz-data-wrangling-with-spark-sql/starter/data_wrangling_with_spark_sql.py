8ecda3f3f6657d88d0b2ee5fa23453dee62fb50f
Dave <dlrobjr@gmail.com>
2025-12-20 12:54:27 -0700

    Completed Question 1 with Spark SQL

--- lesson-2-spark-essentials/exercises/concept5-quiz-data-wrangling-with-spark-sql/starter/data_wrangling_with_spark_sql.py
+++ lesson-2-spark-essentials/exercises/concept5-quiz-data-wrangling-with-spark-sql/starter/data_wrangling_with_spark_sql.py
@@ -15,13 +15,45 @@ from pyspark.sql import SparkSession
 # 4) create a view to use with your SQL queries
 # 5) write code to answer the quiz questions 
 
+spark = SparkSession.builder.appName("Wrangling Quiz with Spark SQL").getOrCreate()
 
+path = "./lesson-2-spark-essentials/exercises/data/sparkify_log_small_2.json"
+df = spark.read.json(path)
+
+df.printSchema()
+
+df.createOrReplaceTempView("user_log_table")
 # # Question 1
 # 
 # Which page did user id ""(empty string) NOT visit?
 
 # TODO: write your code to answer question 1
+print("---------------------------------------------")
+print("Question 1")
+print("---------------------------------------------")
 
+empty_user_query = '''
+          SELECT DISTINCT page AS empty_pages
+          FROM user_log_table
+          WHERE userId == ''
+          '''
+
+all_pages_query = '''
+                         SELECT DISTINCT page
+                         FROM user_log_table
+                         '''
+
+empty_user_df = spark.sql(empty_user_query).show()
+
+all_pages_df = spark.sql(all_pages_query).show()
+
+not_visited_df = spark.sql(f'''
+                           SELECT all_pages.page
+                            FROM ({all_pages_query}) all_pages
+                            LEFT JOIN ({empty_user_query}) empty_pages
+                            ON all_pages.page = empty_pages.empty_pages
+                            WHERE empty_pages.empty_pages IS NULL
+                           ''').show()
 
 # # Question 2 - Reflect
 # 
